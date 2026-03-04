6d3e51ca607eeeba69fdcc070731bebe27f9ff8d
harryamit <amitguptaa9@gmail.com>
2026-03-04 16:04:29 +0530

    added the data asdas added te db

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@
 export const config = {
   port: Number(process.env.PORT) || 3001,
   frontendOrigin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
-  mongodbUri: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/hrms-lite',
+  mongodbUri: process.env.MONGODB_URI ?? 'mongodb+srv://<username>:<db_password>@hrms-lite.vhdxas2.mongodb.net/hrms-lite?retryWrites=true&w=majority',
   jwtSecret: process.env.JWT_SECRET || 'change-me-in-production',
   requireAuth: process.env.REQUIRE_AUTH === 'true',
   retryDbMs: 10000,
