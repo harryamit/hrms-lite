8cac7e82c31d487210d7bafd9a3ec6b2434c3b36
harryamit <amitguptaa9@gmail.com>
2026-03-04 16:24:41 +0530

    added the data asdas added te db dbdbd

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { config } from '../config';
 
 export async function connectDb(): Promise<void> {
+  console.log('Connecting to MongoDB...', config.mongodbUri);
   await mongoose.connect(config.mongodbUri);
 }
 
